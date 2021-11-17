4e6dd938e5a9298f2dda99fa1768b80752270677
Nane <93482631+Kaewdisorn@users.noreply.github.com>
2021-11-18 03:27:53 +0900

    2nd commit

---
+++ STTests/STTests.m
@@ -0,0 +1,40 @@
+//
+//  STTests.m
+//  STTests
+//
+//  Created by Wongsaphat Praisri on 8/7/15.
+//  Copyright (c) 2015 Wongsaphat Praisri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface STTests : XCTestCase
+
+@end
+
+@implementation STTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
